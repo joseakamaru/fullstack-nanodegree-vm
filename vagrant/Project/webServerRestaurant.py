aa604a0a278e3f532116a7a02fcc0ce4d994b251
Jose Medina <joseakamaru@gmail.com>
2019-04-03 23:00:10 -0500

    Added a link to create a new menu item

--- vagrant/Project/webServerRestaurant.py
+++ vagrant/Project/webServerRestaurant.py
@@ -15,13 +15,32 @@ session = DBSession()
 class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path.endswith("/restaurants"):
-            restaurants = session.query(Restaurant).all()
-            output = ""
+
+        # Objective 3 Step 2 - Create /restarants/new page
+        if self.path.endswith("/restaurants/new"):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            output = "<html><body>"
+            output = ""
+            output += "<html><body>"
+            output += "<h1>Make a New Restaurant</h1>"
+            output += "<form method = 'POST' enctype='multipart/form-data'action = '/restaurants/new'>"
+            output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' >"
+            output += "<input type='submit' value='Create'>"
+            output += "</form></body></html>"
+            self.wfile.write(output)
+            return
+
+        elif self.path.endswith("/restaurants"):
+            restaurants = session.query(Restaurant).all()
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            output = ""
+            # Objective 3 Step 1 - Create a Link to create a new menu item
+            output += "<a href = '/restaurants/new' > Make a New Restaurant Here </a>"
+            output += "</br></br>"
+            output += "<html><body>"
             for restaurant in restaurants:
                 output += restaurant.name
                 output += "</br>"
@@ -35,48 +54,26 @@ class WebServerHandler(BaseHTTPRequestHandler):
             print output
             return
 
-        elif self.path.endswith("/hola"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            output = ""
-            output += "<html><body>&#161 Hola !</body></html>"
-            output += '''<form method='POST' enctype='multipart/form-data'
-            action='/hello'><h2>What would you like me to say?</h2><input
-            name="message" type="text" ><input type="submit" value="Submit">
-            </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print message
-            return
-
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
         try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    # Create new Restaurant Object
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
             self.send_response(301)
             self.send_header('Content-type', 'text/html')
+            self.send_header('Location', '/restaurants')
             self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data'
-            action='/hello'><h2>What would you like me to say?</h2><input
-            name="message" type="text" ><input type="submit" value="Submit">
-            </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
         except:
             pass
-
 
 def main():
     try:
